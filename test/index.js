955cea41af639f66a2febee097f10e12e3105fa0
Devin Beliveau <devinmarieb@gmail.com>
2017-01-04 08:56:03 -0700

    Delete extraneous code

---
+++ test/index.js
@@ -0,0 +1,20 @@
+// import App from '../lib/App';
+// import Button from '../lib/Button';
+// import Hint from '../lib/Hint';
+// import Input from '../lib/Input';
+// import Score from '../lib/Score';
+//
+// import { shallow, mount, render } from 'enzyme';
+// import sinon from 'sinon';
+// import { expect } from 'chai';
+// import React from 'react';
+//
+// describe('<App />', () => {
+//
+//   it('should have four Button components', ()=> {
+//     const wrapper = shallow(<App />);
+//     const Button = wrapper.find('Button');
+//     expect (Button).to.have.length(4);
+//   });
+//
+// })
